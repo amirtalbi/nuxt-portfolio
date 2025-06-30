1005fdfc6f701b1a9c216dee055d653c739d451b
amirtalbi <amirotalbi94@gmail.com>
2025-06-30 23:00:08 +0200

    Initialize project

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-[#1A1B1B] text-gray-300 w-full min-h-screen p-0 m-0 scroll-smooth">
+    <NuxtPage class="w-full min-h-screen p-0 m-0" />
+  </div>
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>

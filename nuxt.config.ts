50e75da4953d2377b89c3b875d3a49898f534e1c
Amir <amirotalbi94@gmail.com>
2025-07-01 10:08:38 +0200

    feat: removing netlify configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,24 +4,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/ui', '@nuxtjs/tailwindcss'],
 
-  // Configuration pour Netlify
-  nitro: {
-    preset: 'netlify',
-    prerender: {
-      routes: ['/']
-    }
-  },
-
-  // Configuration SPA pour Netlify
-  ssr: false,
-
-  // Configuration des variables d'environnement
-  runtimeConfig: {
-    public: {
-      baseURL: process.env.NODE_ENV === 'production' ? 'https://amirtalbi.me' : 'http://localhost:3000'
-    }
-  },
-
   // Configuration du CSS
   css: ['~/assets/css/style.css']
 })
